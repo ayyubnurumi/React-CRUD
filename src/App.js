ed65d4a4c3080ee951f8af85b288d9ecf9647f33
ayyubnurumi <ayyubinvestment@gmail.com>
2022-12-22 18:46:56 +0700

    CRUD2

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
-import { Create } from "./components/create";
-import { Read } from "./components/read";
-import { Update } from "./components/update";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Create } from "./components-crud1/create";
+import { Read } from "./components-crud1/read";
+import { Update } from "./components-crud1/update";
 import "./App.css";
-
-const LayOut = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React CRUD Operations</h1>
-        <ul>
-          <li><a href="/create">create</a></li>
-          <li><a href="/read">read</a></li>
-        </ul>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  );
-};
+import { TutorialsList } from "./components-crud2/TutorialsList";
+import { AddTutorial } from "./components-crud2/AddTutorial";
+import { Tutorial } from "./components-crud2/Tutorial";
+import { Home } from "./Home";
 
 function App() {
+  
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LayOut />}>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          bezKoder
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
           <Route path="/create" element={<Create />} />
-          <Route index path="/read" element={<Read />} />
+          <Route path="/read" element={<Read />} />
           <Route path="/update" element={<Update />} />
-          <Route path="/" element={<Navigate to="/read" />} replace />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/" element={<Home />} />
+          <Route path="/tutorials" element={<TutorialsList />} />
+          <Route path="/add" element={<AddTutorial />} />
+          <Route path="/tutorials/:id" element={<Tutorial />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
