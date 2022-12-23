8cc6616bd248d24fa94892cef64baf011dae8129
ayyubnurumi <ayyubinvestment@gmail.com>
2022-12-23 11:32:16 +0700

    make home

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,17 @@ export const Home = () => {
     <div className='flex-container'>
       <div className='flex-item bg-one tilt'>
         <a href='-'>
+          <span>
+            <img src='' alt='' />
+          </span>
           <h1 className='text'>crud1</h1>
         </a>
       </div>
       <div className='flex-item bg-two tilt'>
         <a href='-'>
+          <span>
+            <img src='' alt='' />
+          </span>
           <h1 className='text'>crud2</h1>
         </a>
       </div>
